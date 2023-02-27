49a7eede14cda63c9929c5adb73a37185783bddf
Ítalo Góes <italogoes85@gmail.com>
2023-02-26 23:10:50 -0300

    desfazendo o teste

--- portfolio/src/components/Intro/Intro.jsx
+++ portfolio/src/components/Intro/Intro.jsx
@@ -13,7 +13,7 @@ const Intro = (props) => {
                 <h1 className='message-animate'>Ítalo Góes</h1>
                 <p className='oq-sou'>Desenvolvedor Full Stack</p>
                 <div className='btns-curriculo-contato'>
-                    <a className='btn-curriculo' href="">Download CVvvvvvvv</a>
+                    <a className='btn-curriculo' href="">Download CV</a>
                     <a className='btn-contato' href="">Entrar em contato</a>
                 </div>
             </div>
