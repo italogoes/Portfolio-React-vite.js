6ec0e4056a06d44e42bbc06b7ccd40f6dcaa11ce
Ítalo Góes <italogoes85@gmail.com>
2023-03-09 12:59:16 -0300

    ajustes nas redes sociais

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
             <p>Desenvolvido por Ítalo Góes</p>
             
             <div>
-                <a href="https://www.linkedin.com/in/italo-goes">
+                <a href="https://www.linkedin.com/in/italo-goes" target='_blank'>
                     <img src={linkedin} alt="" />
                 </a>
-                <a href="https://www.linkedin.com/in/italo-goes">
+                <a href="https://api.whatsapp.com/send?phone=5519837410047&text=Ol%C3%A1,%20%C3%8Dtalo!%20Estou%20entrando%20em%20contato%20atrav%C3%A9s%20do%20seu%20portf%C3%B3lio" target='_blank'>
                     <img src={whatsapp} alt="" />
                 </a>
             </div>
